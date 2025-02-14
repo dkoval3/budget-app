6e96a47115e4f654366b2faf0a81fc3829ac5e61
Dane <dkoval3@gatech.edu>
2025-02-14 11:21:59 -0700

    Finish budget table checkbox logic

--- src/app/components/BudgetPage/BudgetContainer/BudgetPanel/BudgetTable/BudgetTable.tsx
+++ src/app/components/BudgetPage/BudgetContainer/BudgetPanel/BudgetTable/BudgetTable.tsx
@@ -1,20 +1,74 @@
 'use client'
 
-import {Budget, BudgetLineItem, budgetToLineItemList} from "@/model/BudgetTypes";
+import {Budget, BudgetLineItem, generateCategoryLineItem} from "@/model/BudgetTypes";
 import {formatAsDollarAmount} from "@/common/Formatter";
 import {useImmer} from "use-immer";
+import {useState} from "react";
+import {WritableDraft} from "immer";
 
 const cellStyle = '';
 const rowStyle = '';
 
 export default function BudgetTable({ budget, className }: BudgetTableProps) {
-    const [budgetList, updateBudgetList] = useImmer(budgetToLineItemList(budget));
+    const [budgetObject, updateBudgetObject] = useImmer(budget);
+    const [headerIsSelected, setHeaderIsSelected] = useState(false);
+
+    const BudgetCell = ({className, message, includeCheckbox = false, isSelected = false, onClick = () => {}}: BudgetCellProps) => (
+        <td className={`${className} ${cellStyle}`}>
+            {includeCheckbox ? <input checked={isSelected} onClick={onClick} onChange={() => {}} type='checkbox' className='mx-2' /> : null}
+            {message}
+        </td>
+    );
+
+    const BudgetRow = ({ item, isSelected, onClick, className }: BudgetRowProps) => (
+        <tr className={`${className} ${rowStyle} text-right border-[0.5px] border-gray-700`}>
+            <BudgetCell includeCheckbox={true} onClick={onClick} isSelected={isSelected} className='text-left' message={item.lineItem}/>
+            <BudgetCell message={formatAsDollarAmount(item.assigned)}/>
+            <BudgetCell message={formatAsDollarAmount(item.activity)}/>
+            <BudgetCell message={formatAsDollarAmount(item.assigned - item.activity)}/>
+        </tr>
+    );
 
     const switchAllBoxes = () => {
-        updateBudgetList(draft => {
-            draft.forEach(item => item.isSelected = !item.isSelected);
+        updateBudgetObject(draft => {
+            draft.forEach(budgetCategory => {
+                budgetCategory.isSelected = !headerIsSelected;
+                budgetCategory.lineItems.forEach(lineItem => {
+                    lineItem.isSelected = !headerIsSelected;
+                });
+            });
+            setHeaderIsSelected(!headerIsSelected);
         })
     };
+
+    const switchCategoryBoxes = (i: number) => {
+        updateBudgetObject(draft => {
+            draft[i].isSelected = !draft[i].isSelected;
+            draft[i].lineItems.forEach(lineItem => {
+                lineItem.isSelected = draft[i].isSelected;
+            });
+            updateHeaderIsSelected(draft);
+        });
+    };
+
+    const switchBox = (i: number, j: number) => {
+        updateBudgetObject(draft => {
+            draft[i].lineItems[j].isSelected = !draft[i].lineItems[j].isSelected;
+            if (draft[i].lineItems[j].isSelected) {
+                draft[i].isSelected = true
+            } else if (!draft[i].lineItems.some(item => item.isSelected)) {
+                draft[i].isSelected = false;
+            }
+            updateHeaderIsSelected(draft);
+        });
+    }
+
+    const updateHeaderIsSelected = (budgetDraft: WritableDraft<Budget>) => {
+        const anySelected = budgetDraft.some(category => category.isSelected);
+        setHeaderIsSelected(anySelected);
+    }
+
+    const onCheckboxClick = (i: number, j: number, isCategoryHeader: boolean) => isCategoryHeader ? switchCategoryBoxes(i) : switchBox(i, j);
 
     return (
         <div className='overflow-auto w-full'>
@@ -22,7 +76,11 @@ export default function BudgetTable({ budget, className }: BudgetTableProps) {
                 <thead>
                     <tr className={`${rowStyle}`}>
                         <th className='text-left'>
-                            <input onClick={() => switchAllBoxes()} type="checkbox" className='mx-2' />
+                            <input
+                                onClick={() => switchAllBoxes()}
+                                checked={headerIsSelected}
+                                type="checkbox"
+                                className='mx-2' />
                             Category
                         </th>
                         <th className='text-right'>Assigned</th>
@@ -32,32 +90,22 @@ export default function BudgetTable({ budget, className }: BudgetTableProps) {
                 </thead>
                 <tbody>
                 {
-                    budgetList.map((lineItem, i) => (
-                        lineItem.isCategoryHeader
-                            ? <BudgetRow key={i} item={lineItem} isSelected={lineItem.isSelected} className='font-bold text-lg bg-sidebarBackground' />
-                            : <BudgetRow key={i} item={lineItem} isSelected={lineItem.isSelected} />
-                    ))
+                    budgetObject.map((budgetCategory, i) => {
+                        const categoryLineItem = generateCategoryLineItem(budgetCategory);
+                        return [categoryLineItem, ...budgetCategory.lineItems].map((lineItem, j) => {
+                            return (
+                                lineItem.isCategoryHeader
+                                    ? <BudgetRow onClick={() => onCheckboxClick(i, j, true)} key={`${i},${j}`} item={lineItem} isSelected={lineItem.isSelected} className='font-bold text-lg bg-sidebarBackground' />
+                                    : <BudgetRow onClick={() => onCheckboxClick(i, j - 1, false)} key={`${i},${j}`} item={lineItem} isSelected={lineItem.isSelected} />
+                            );
+                        });
+                    })
                 }
                 </tbody>
             </table>
         </div>
     );
 }
-
-const BudgetRow = ({ item, isSelected, className }: BudgetRowProps) => (
-    <tr className={`${className} ${rowStyle} text-right border-[0.5px] border-gray-700`}>
-        <BudgetCell includeCheckbox={true} isSelected={isSelected} className='text-left' message={item.lineItem}/>
-        <BudgetCell message={formatAsDollarAmount(item.assigned)}/>
-        <BudgetCell message={formatAsDollarAmount(item.activity)}/>
-        <BudgetCell message={formatAsDollarAmount(item.assigned - item.activity)}/>
-    </tr>
-);
-
-const BudgetCell = ({className, message, includeCheckbox = false, isSelected = false}: BudgetCellProps) =>
-    <td className={`${className} ${cellStyle}`}>
-        {includeCheckbox ? <input checked={isSelected} onChange={() => {}} type='checkbox' className='mx-2' /> : null}
-        {message}
-    </td>;
 
 interface BudgetTableProps {
     budget: Budget,
@@ -68,6 +116,7 @@ interface BudgetRowProps {
     item: BudgetLineItem,
     className?: string,
     isSelected?: boolean,
+    onClick?: () => void,
 }
 
 interface BudgetCellProps {
@@ -75,4 +124,5 @@ interface BudgetCellProps {
     message: string | number,
     includeCheckbox?: boolean,
     isSelected?: boolean,
+    onClick?: () => void,
 }
--- src/model/BudgetTypes.ts
+++ src/model/BudgetTypes.ts
@@ -3,6 +3,7 @@ export type Budget = BudgetCategory[];
 export interface BudgetCategory {
     categoryName: string,
     lineItems: BudgetLineItem[],
+    isSelected: boolean,
 }
 
 export class BudgetLineItem {
@@ -19,13 +20,8 @@ export class BudgetLineItem {
     }
 }
 
-function budgetLineItemToObject(lineItem: BudgetLineItem) {
-    return {
-        lineItem: lineItem.lineItem,
-        assigned: lineItem.assigned,
-        activity: lineItem.activity,
-        isCategoryHeader: lineItem.isCategoryHeader ?? false,
-    };
+export function newBudgetLineItem(): BudgetLineItem {
+    return { lineItem: '', assigned: 0, activity: 0, isSelected: false, isCategoryHeader: false };
 }
 
 export function budgetToLineItemList(budget: Budget): BudgetLineItem[] {
@@ -36,8 +32,8 @@ export function budgetToLineItemList(budget: Budget): BudgetLineItem[] {
     })
 }
 
-function generateCategoryLineItem(budgetCategory: BudgetCategory): BudgetLineItem {
-    const categoryLineItem = { lineItem: budgetCategory.categoryName, assigned: 0, activity: 0, isSelected: false, isCategoryHeader: true };
+export function generateCategoryLineItem(budgetCategory: BudgetCategory): BudgetLineItem {
+    const categoryLineItem = { lineItem: budgetCategory.categoryName, assigned: 0, activity: 0, isSelected: budgetCategory.isSelected, isCategoryHeader: true };
     return budgetCategory.lineItems.reduce((previousVal, currentValue) => {
         previousVal.activity += currentValue.activity;
         previousVal.assigned += currentValue.assigned;
@@ -48,6 +44,7 @@ function generateCategoryLineItem(budgetCategory: BudgetCategory): BudgetLineIte
 export const sampleBudget: Budget = [
     {
         categoryName: 'Needs',
+        isSelected: false,
         lineItems: [
             {lineItem: 'Rent', assigned: 1800, activity: 1601, isSelected: false },
             {lineItem: 'Apartment', assigned: 400, activity: 150, isSelected: false},
@@ -56,6 +53,7 @@ export const sampleBudget: Budget = [
     },
     {
         categoryName: 'Wants',
+        isSelected: false,
         lineItems: [
             {lineItem: 'Restaurants', assigned: 300, activity: 143.23 , isSelected: false},
             {lineItem: 'Fun', assigned: 350, activity: 134, isSelected: false},
